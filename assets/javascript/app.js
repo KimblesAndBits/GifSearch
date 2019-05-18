7c3f68290c26c2fe1772fe00c509eaa8518a74ac
KimblesAndBits <wearethekimbles@gmail.com>
2019-05-18 14:37:34 -0700

    added css, working click to animate, reset button and check box to replace or keep the gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,11 +25,24 @@ $(document.body).on("click", ".topic-button", function () {
         method: "GET"
       }).then(function(response) {
           var gifList = response.data;
+          if(!$("#maintain").prop("checked")) {
+                $("#gif-area").empty();
+            }
           gifList.forEach(function(element){
-            var newGif = $(`<div class="col-md-3"><img src="${element.images.original_still.url}" alt="${element.slug}" width="100%"><p>Rating: ${element.rating}</p></div>`);
+            var newGif = $(`<div class="col-md-3"><div class="card" style="width: 100%;"><img src="${element.images.original_still.url}" class="card-img-top gif-card" alt="${element.slug}" data-playing="${element.images.original.url}" data-stopped="${element.images.original_still.url}" data-state="stopped"><h5 class="card-title">Title: "${element.title}"</h5><p class="card-text">Rating: ${element.rating}</p></div>`);
             $("#gif-area").prepend(newGif);
           });
       });
+});
+
+$(document.body).on("click", ".gif-card", function () {
+    if($(this).attr("data-state") === "stopped"){
+        $(this).attr("data-state", "playing");
+        $(this).attr("src", $(this).attr("data-playing"));
+    } else {
+        $(this).attr("data-state", "stopped");
+        $(this).attr("src", $(this).attr("data-stopped"));
+    };
 });
 
 $("#limit-button").on("click", function () {
@@ -40,11 +53,19 @@ $("#limit-button").on("click", function () {
 
 $("#search-button").on("click", function () {
     event.preventDefault();
-    var newTerm = $("#search-term").val()
+    var newTerm = $("#search-term").val();
+    $("#search-term").val("");
     topics.push(newTerm);
     localStorage.setItem("topics", JSON.stringify(topics));
     $("#button-area").empty();
     renderButtons();
 });
 
+$("#reset-list").on("click", function () {
+    event.preventDefault();
+    localStorage.clear();
+    $("#button-area").empty();
+    topics = ["The Legend of Zelda", "Super Smash Brothers", "Nintendo Switch", "Apple Computers", "Playstation", "Coding", "The Big Lebowski", "The Decemberists", "The Office", "Parks and Rec", "The Good Place", "Star Wars"];
+    renderButtons();
+});
 });
