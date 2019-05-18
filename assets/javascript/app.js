30cce236c80f1e83d15beb57ba7498be038366ed
KimblesAndBits <wearethekimbles@gmail.com>
2019-05-18 11:38:09 -0700

    shows the gifs it found

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function (){
-var topics = ["The Legend of Zelda", "Super Smash Brothers", "Dragon Quest XI", "Apple", "Playstation", "Coding", "The Big Lebowski", "The Decemberists", "The Office", "Parks and Rec", "The Good Place", "Star Wars"];
+var topics = ["The Legend of Zelda", "Super Smash Brothers", "Nintendo Switch", "Apple Computers", "Playstation", "Coding", "The Big Lebowski", "The Decemberists", "The Office", "Parks and Rec", "The Good Place", "Star Wars"];
 if(typeof localStorage.topics !== "undefined"){
     topics = localStorage.getItem("topics");
 };
-var apiKey = "api_key=fjZDB3KgrG7UOjBcD3hTIaQfUsIemsu6&q=";
+var apiKey = "api_key=fjZDB3KgrG7UOjBcD3hTIaQfUsIemsu6";
 var gifLimit = 10;
 var limitURL = "&limit=";
 var domain = "https://api.giphy.com/v1/gifs/search?";
@@ -18,7 +18,19 @@ function renderButtons() {
 }
 
 $(document.body).on("click", ".topic-button", function () {
-    
+    var query = `&q=${$(this).val()}`
+    var queryURL = domain + apiKey + query + limitURL + gifLimit;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+          console.log(response);
+          var gifList = response.data;
+          gifList.forEach(function(element){
+            var newGif = $(`<div class="col-md-3"><img src="${element.images.original_still.url}" alt="${element.slug}" width="100%"><p>Rating: ${element.rating}</p></div>`);
+            $("#gif-area").prepend(newGif);
+          });
+      });
 });
 
 });
